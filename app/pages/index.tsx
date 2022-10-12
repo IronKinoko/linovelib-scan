bb940be7f8b565b6806621a0856af7de02059c9a
IronKinoko <kinoko_main@outlook.com>
2022-10-12 14:08:46 +0800

    feat: search input auto focus

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -16,6 +16,7 @@ const Home: NextPage = () => {
             type="search"
             defaultValue={bookId}
             enterKeyHint="search"
+            autoFocus
             onKeyDown={(e) => {
               if (e.key === 'Enter') router.replace({ query: { bookId: e.currentTarget.value } })
             }}
