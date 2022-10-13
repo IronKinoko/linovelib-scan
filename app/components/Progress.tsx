4a564ece34ee8a0a29676a3994d11f4b2ed433be
IronKinoko <kinoko_main@outlook.com>
2022-10-13 22:18:37 +0800

    chore: adjust progress ease

--- app/components/Progress.tsx
+++ app/components/Progress.tsx
@@ -16,7 +16,7 @@ const Progress: FC<{ progress?: FetchProgress }> = ({ progress }) => {
       )}
       {(status === 'asset' || status === 'done') && (
         <div
-          className="absolute inset-0 bg-sky-400/10 w-0 transition-all"
+          className="absolute inset-0 bg-sky-400/10 w-0 transition-all ease-linear"
           style={{
             width: (assets / totalAssets + downloadProgress) * 50 + '%',
           }}
